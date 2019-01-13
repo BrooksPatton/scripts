b90ab99cbfb7fd8b16ec93979792d233cfac3c87
Brooks Patton <brooks@brookzerker.com>
2019-01-13 15:20:40 -0700

    wip

--- set_up_debian.sh
+++ set_up_debian.sh
@@ -5,7 +5,13 @@ sudo apt-get update && sudo apt-get -y install \
 	libasound2-dev \
 	libsdl2-dev \
 	pkg-confi \
-	dropboxg
+	dropboxg \
+	tmux
+	apt-transport-https \
+	ca-certificates \
+	gnupg2 \
+	curl \
+	software-properties-common
 
 curl https://sh.rustup.rs -sSf | sh
 
@@ -17,3 +23,10 @@ git config --global user.name "$GITHUB_NAME"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 dropbox start -i
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+	"deb [arch=amd64] https://download.docker.com/linux/debian \
+	$(lsb_release -cs) \
+	stable"
+sudo apt-get update
